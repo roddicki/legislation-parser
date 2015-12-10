e0ddddc1594a1cdf556c7dc80dc3117c3e6376fa
Tom Keene <tom@theanthillsocial.co.uk>
2015-12-10 16:10:57 +0000

    Added basic structure for parsing live feed

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # legislation-parser
 legislation, parse legislation data, atom parser
+
+Parses data provided by: www.legislation.gov.uk
+
+## Parsing the atom feed
+*Latest legislation is published here:* http://www.legislation.gov.uk/new/data.feed
+
+This feed provides the following entries:
+ 
+       <entry>
+            <id>http://www.legislation.gov.uk/id/nisr/2015/404</id>
+            <title>The Social Security (Crediting and Treatment of Contributions, and National Insurance Numbers) (Amendment) Regulations (Northern Ireland) 2015</title>
+            <link rel="self" href="http://www.legislation.gov.uk/id/nisr/2015/404"/>
+            <link href="http://www.legislation.gov.uk/nisr/2015/404/made"/>
+            <link rel="alternate" type="application/xml" href="http://legislation.data.gov.uk/nisr/2015/404/made/data.xml" title="XML"/><link rel="alternate" type="application/rdf+x
+            <link rel="http://purl.org/dc/terms/tableOfContents" type="application/xml" href="http://www.legislation.gov.uk/nisr/2015/404/contents/made" title="Table of Contents"/>
+            <author><name/></author>
+            <updated>2015-12-10T14:38:03.439Z</updated>
+            <published>2015-12-10T14:16:51.818Z</published>
+            <ukm:DocumentMainType Value="NorthernIrelandStatutoryRule"/>
+            <ukm:Year Value="2015"/>
+            <ukm:Number Value="404"/>
+            <ukm:ISBN Value="9780337999222"/>
+            <ukm:CreationDate Date="2015-12-07"/>
+            <ukm:Subject Value="SOCIAL SECURITY"/>  
+            <category term="SOCIAL SECURITY"/>
+            <summary>These Regulations amend the Social Security (Crediting and Treatment of Contributions, and National Insurance Numbers) Regulations (Northern Ireland) 2001 (“the
+        </entry>
+
+
--- test.py
+++ test.py
@@ -1,9 +1,53 @@
-#! /bin/python
-
+#! /bin/python2
+import xml.etree.ElementTree as ET
 from datetime import datetime
-now = str(datetime.now())+"\n"; 
 
-print("hello")
-f = open('test.txt','w')
-f.write(now) # python will convert \n to os.linesep
-f.close()
+# Class to grab legislation rss feed
+class LegislationParser:
+
+    def __init__(self, level):
+        self.level = level        
+        self.debug("Started")
+        self.grabatomfeed()
+        self.parsexml()
+        self.output()
+    
+    def grabatomfeed(self):
+        self.debug("Grabbing atom feed")
+
+    def parsexml(self):
+        self.debug('parsing xml')
+        tree = ET.parse('atomdata.feed')
+        root = tree.getroot()
+        #print(root)
+        ns = "{http://www.w3.org/2005/Atom}"
+        for entry in root.findall(ns+"entry"):
+            data = {'id':'','published':''}
+            # {http://www.w3.org/2005/Atom}published
+            # {http://www.legislation.gov.uk/namespaces/metadata}DocumentMainType
+            data['id'] = entry.find(ns+'id').text
+            data['title'] = entry.find(ns+'title').text
+            data['updated'] = entry.find(ns+'updated').text
+            data['published'] = entry.find(ns+'published').text
+            data['DocumentMainType'] = entry.find(ns+'published').text
+
+            # data['summary'] = entry.find(ns+'published').text
+            print(data)
+            #att = str(child.attrib)
+    
+    def parsepigskin():
+        self.debug("Gr")
+
+    def output(self):
+        self.debug('output')
+        now = str(datetime.now())+"\n" 
+        f = open('test.txt','w')
+        f.write(now)  
+        f.close()     
+        
+    def debug(self, txt, level='debug'):
+        print(txt)
+        
+
+if __name__ == "__main__": 
+    lp = LegislationParser('debug');
