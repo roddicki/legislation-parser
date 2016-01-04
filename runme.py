1dff55c23035968222b3e022f42be1f67213088d
rod <rod@roddickinson.net>
2016-01-04 15:38:00 +0000

    changed how global legislation object init

--- runme.py
+++ runme.py
@@ -5,7 +5,13 @@ from make_json import makeJSON
 
 
 # Global object to parse legislation data
-legi = LegislationParser()
+# won't run on server - uncomment to run locally
+#legi = LegislationParser()
+
+# Global object to parse legislation data
+# won't run locally - comment out to run locally
+db = "/home4/roddicki/legislation-parser/data/db.sqlite3"
+legi = LegislationParser(db)
 
 def run():
     # Save the latest legislation entries from:
